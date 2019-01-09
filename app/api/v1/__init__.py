2b1e71d732c7a325f1407900d2c598f57e4da352
Maggie Kimani <maggiekim42@gmail.com>
2019-01-09 23:22:06 +0300

    [Feature #163051774] Writing an endpoint for RSVP

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from app.api.v1.views.meetups import AllMeetupsApi, SingleMeetupApi
+from app.api.v1.views.meetups import AllMeetupsApi, SingleMeetupApi, RsvpApi
 from app.api.v1.views.questions import AllQuestionsApi
 
 
@@ -10,4 +10,5 @@ api_v1 = Api(app_v1)
 
 api_v1.add_resource(AllMeetupsApi, '/meetups/upcoming')
 api_v1.add_resource(SingleMeetupApi, '/meetups/<int:meetup_id>')
+api_v1.add_resource(RsvpApi, '/meetups/<int:meetup_id>/rsvps/<int:rsvp_id>')
 api_v1.add_resource(AllQuestionsApi, '/questions')
--- app/api/v1/views/meetups.py
+++ app/api/v1/views/meetups.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource
-from app.api.v1.models.meetupsmodel import all_meetups, Meetups
+from app.api.v1.models.meetupsmodel import all_meetups, Meetups, all_rsvps
 
 
 class AllMeetupsApi(Resource):
@@ -10,27 +10,35 @@ class AllMeetupsApi(Resource):
         data = request.get_json()
 
         if not data:
-            return "No meetup record found", 404
+            return "Data must be in JSON format", 404
         try:
             meetup_id = len(all_meetups) + 1
-            createdOn = data["createdOn"]
             location = data["location"]
             topic = data["topic"]
 
-            meetup_record = Meetups(meetup_id, createdOn,
-                                    location, topic).create_meetup()
+            meetup_record = Meetups().create_meetup(meetup_id,
+                                                    location, topic)
+            print(all_meetups)
+            print(meetup_record)
 
-            response = jsonify({"status": 201,
-                                "data": meetup_record})
-            response.status_code = 201
-        except:
+            if meetup_record:
+                response = jsonify({"status": 201,
+                                    "data": meetup_record})
+                response.status_code = 201
+                print(all_meetups)
+                return response
+            else:
+                return 'Could not create meetup', 400
+
+        except Exception as e:
             return "PLease include all details", 400
-        return response
+            # print(e)
 
     def get(self):
-        '''Endpoint for getting all meetup records'''
+        '''Endpoint for geting all meetup records'''
 
-        meetups = Meetups.get_all_meetups(self)
+        meetups = Meetups().get_all_meetups()
+        # print(all_meetups)
         response = jsonify({"status": 200,
                             "data": meetups})
         response.status_code = 200
@@ -41,8 +49,36 @@ class SingleMeetupApi(Resource):
     '''Endpoint for single meetup functionality'''
 
     def get(self, meetup_id):
-        single_meetup = Meetups.get_one_meetup(self, meetup_id)
-        response = jsonify({"status": 200,
-                            "data": single_meetup})
-        response.status_code = 200
-        return response
+        meetup_available = Meetups().get_one_meetup(meetup_id)
+        rsvp_available = [
+            rsvp for rsvp in all_rsvps if rsvp['meetup_id'] == meetup_id
+        ]
+        if meetup_available:
+            return {'Meetup': meetup_available,
+                    'RSVPs': rsvp_available}, 200
+        return {"message": "Meetup not found"}, 404
+
+    def post(self, meetup_id):
+        '''Post an RSVP'''
+
+        try:
+            data = request.get_json()
+            status = data['status']
+        except:
+            return 'Check input data', 400
+
+        meetup_available = Meetups().get_one_meetup(meetup_id)
+        print(meetup_available)
+        if not meetup_available:
+            return "You cannot RSVP an unavailable meetup", 400
+
+        new_rsvp = Meetups().create_rsvp(meetup_id, status)
+
+        if not new_rsvp:
+            return {"Message": 'RSVP could not be saved'}, 400
+
+        return {"Message": 'RSVP saved for meetup {} successfully'.format(meetup_id)}, 200
+
+
+class RsvpApi(Resource):
+    '''Endpoint for Rsvps'''
